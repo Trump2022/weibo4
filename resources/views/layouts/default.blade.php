27816444ef6cc222449ba385b2574b9d3e51017d
Trump2022 <qsw2012@outlook.com>
2022-03-30 21:11:23 +0800

    静态文件浏览器缓存问题

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>@yield('title', 'Weibo App') - Laravel 新手入门教程</title>
-  <link rel="stylesheet" href="/css/app.css">
+  <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
+  <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
 
@@ -19,6 +19,5 @@
 <div class="container">
   @yield('content')
 </div>
-
 </body>
 </html>
